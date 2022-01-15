9138671956881b0bbd37da18cf159ab4ff57604c
@InCorCadit <rosca.alexandru@isa.utm.md>
2022-01-15 18:01:04 +0200

    setup heroku app

--- backend/src/core/db.ts
+++ backend/src/core/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb://localhost:27017/chat",
+  "mongodb+srv://incorcadit:fNd34Rzw-@cluster0.mxwfj.gcp.mongodb.net/chat?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
