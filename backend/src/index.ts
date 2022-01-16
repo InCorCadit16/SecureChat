2ffe737e0535058f5f96553cc5b1091775166206
@InCorCadit <rosca.alexandru@isa.utm.md>
2022-01-16 13:07:49 +0200

    homeak-huiak

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,13 +7,13 @@ dotenv.config();
 import './core/db';
 import createRoutes from './core/routes';
 import createSocket from './core/socket';
-
+console.log(`huiak`);
 const app = express();
 const http = createServer(app);
 const io = createSocket(http);
 
 createRoutes(app, io);
-
+console.log(`homeak`);
 const PORT: number = process.env.PORT ? Number(process.env.PORT) : 3003;
 
 http.listen(PORT, function () {
