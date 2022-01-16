db6249850d33da0ec005715509b0f504fdc8c5b6
@InCorCadit <rosca.alexandru@isa.utm.md>
2022-01-16 02:27:40 +0200

    changed user password

--- backend/src/core/db.ts
+++ backend/src/core/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://incorcadit:fNd34Rzw-@cluster0.mxwfj.gcp.mongodb.net/chat?retryWrites=true&w=majority",
+  "mongodb+srv://incorcadit:alex079887923@cluster0.mxwfj.gcp.mongodb.net/chat?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
