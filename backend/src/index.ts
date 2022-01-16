152b5b50e310c8b00125dd95411281496fb7455e
@InCorCadit <rosca.alexandru@isa.utm.md>
2022-01-16 17:04:23 +0200

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,13 +7,16 @@ dotenv.config();
 import './core/db';
 import createRoutes from './core/routes';
 import createSocket from './core/socket';
-console.log(`huiak`);
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const http = createServer(app);
 const io = createSocket(http);
 
 createRoutes(app, io);
-console.log(`homeak`);
 const PORT: number = process.env.PORT ? Number(process.env.PORT) : 3003;
 
 http.listen(PORT, function () {
